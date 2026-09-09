06237878de3e322f5009ec520460414801226574
tozi-coli <kirich.beas+public@gmail.com>
2026-09-09 21:32:51 +0200

    i didnt know it was case sensitive geez

--- datingsim.js
+++ datingsim.js
@@ -1137,7 +1137,7 @@ span.bouncy {
     // audio
 
     blipsSfx: new Howl({
-        src: ["https://tozi-coli.github.io/courtu.observer/blips.wav"],
+        src: ["https://tozi-coli.github.io/COURTu.observer/blips.wav"],
         preload: true,
         volume: 0.75,
         rate: 1,
